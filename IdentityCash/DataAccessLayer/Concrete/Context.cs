4f7708887d3a4195983e0317b12a4c1b659ea507
trkhmrt <trkhamarat@gmail.com>
2024-01-29 01:56:51 +0300

    .

--- IdentityCash/DataAccessLayer/Concrete/Context.cs
+++ IdentityCash/DataAccessLayer/Concrete/Context.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace DataAccessLayer.Concrete
 {
-	public class Context
+	public class Context:IdentityDbContext<AppUser,AppRole,int>
 	{
-		
-	}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = localhost; Database = IdentityCashDB; Uid = SA; Password =reallyStrongPwd123;");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+
+
+    }
 }
 
---
+++ IdentityCash/EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace EntityLayer.Concrete
+{
+	public class AppUser:IdentityUser<int>
+	{
+		public string Name { get; set; }
+
+		public string Surname { get; set; }
+
+		public string District { get; set; }
+
+		public string City { get; set; }
+
+		public string ImageUrl { get; set; }
+
+
+	}
+}
+
