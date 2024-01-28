1d114a46f397151762db2c5224c23bedb1461fab
trkhmrt <trkhamarat@gmail.com>
2024-01-29 02:38:41 +0300

    .

---
+++ IdentityCash/BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DtoLayer.AppUserDtos;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules.AppUserValidationRules
+{
+	public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
+	{
+
+
+		public AppUserRegisterValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+        }
+
+
+	}
+}
+
